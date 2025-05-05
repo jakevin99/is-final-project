6ee12873f170f2e6ec196354e4feb0c4985e03b8
Jedd Niñonuevo <jed@jasetech.ca>
2025-05-05 12:58:54 +0800

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { Card } from "flowbite-svelte";
+</script>
+
+<main class="flex h-screen w-screen flex-col items-center justify-center p-4">
+	<Card size="lg">Test</Card>
+</main>
